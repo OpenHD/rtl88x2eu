89f38a1594c25c9e4c37a7a22446d25e096d078d
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2026-01-16 00:22:37 +0100

    add SNR

--- core/rtw_recv.c
+++ core/rtw_recv.c
@@ -103,6 +103,7 @@ sint _rtw_init_recv_priv(struct recv_priv *precvpriv, _adapter *padapter)
 #else
 	precvpriv->store_law_data_flag = 0;
 #endif
+	_rtw_memset(precvpriv->ofdm_snr_latest, 0, sizeof(precvpriv->ofdm_snr_latest));
 
 	rtw_os_recv_resource_init(precvpriv, padapter);
 
